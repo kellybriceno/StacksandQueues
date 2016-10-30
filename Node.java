9692bb01f3c8384b3716c853a93f3221d507f504
kelly briceno <kellygavin96@gmail.com>
2016-10-30 12:55:36 -0400

    initial commit

---
+++ Node.java
@@ -0,0 +1,21 @@
+//kelly briceno 
+//Stacks and Queue homework
+//ICSI 213 
+class Node {
+   //properties
+ public String data; //data in node
+  public Node previous; //points to next node in list
+  public Node next; //points to previous node in list 
+  
+  //Constructors
+  public Node(String data) {
+    this.data = data;
+    
+  } //
+  
+  //display nodes data
+  public void displayNode() {
+    System.out.print(data + "");
+  }
+
+}
